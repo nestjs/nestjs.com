a672141cc4aa1a0f83989ad862f1aa17a5a1128a
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-03-13 23:03:48 +0100

    feature(): update video (use jscamp presentation), add smooth scroll

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ window.addEventListener('load', function() {
   var offset = 300;
 
   toggleStickyNavbar();
+
   window.addEventListener('scroll', function() {
     toggleStickyNavbar();
   });
@@ -21,4 +22,28 @@ window.addEventListener('load', function() {
     var videoWrapper = document.querySelector('.plyr__video-wrapper');
     videoWrapper.classList.add('hidden-poster');
   });
+
+  registerNavigation();
+
+  function registerNavigation() {
+    var elements = document.querySelectorAll('.nav-wrapper a');
+    elements.forEach(function(el) {
+      const href = el.getAttribute('href') || '';
+      const isAnchor = href[0] === '#';
+      if (!isAnchor) {
+        return;
+      }
+      el.addEventListener('click', function(event) {
+        if (!el.scrollIntoView) {
+          return;
+        }
+        event.preventDefault();
+
+        var targetElement = document.querySelector(el.getAttribute('href'));
+        if (targetElement) {
+          targetElement.scrollIntoView({ block: 'start', behavior: 'smooth' });
+        }
+      });
+    });
+  }
 });
