642a62cc84ef2ccb73ff2018573898028caa4c21
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-06 13:15:30 +0200

    feat() newsletter update

--- scripts.js
+++ scripts.js
@@ -46,4 +46,28 @@ window.addEventListener('load', function() {
       });
     });
   }
+
+  const newsletterAddButton = document.querySelector('.newsletter-form button');
+  const newsletterEmailInput = document.querySelector('#newsletter-email');
+  const formRef = document.querySelector('.newsletter-form form');
+  if (newsletterAddButton && newsletterEmailInput) {
+    formRef.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      newsletterAddButton.setAttribute('disabled', 'disabled');
+      newsletterEmailInput.setAttribute('disabled', 'disabled');
+
+      const value = newsletterEmailInput.value;
+      const xhr = new XMLHttpRequest();
+      const url =
+        'https://gno1bdchc9.execute-api.us-east-2.amazonaws.com/Stage/newsletter?token=db1f899025b5a59a76b6b34b2a013893';
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onreadystatechange = function() {
+        newsletterAddButton.classList.add('btn-success');
+      };
+      const data = JSON.stringify({ email: value });
+      xhr.send(data);
+    });
+  }
 });
