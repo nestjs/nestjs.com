9b1bd6be567b45d293f62bf101ea9e01316a5322
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-09 18:30:36 +0200

    fix() fix newsletter URL

--- scripts.js
+++ scripts.js
@@ -60,7 +60,7 @@ window.addEventListener('load', function() {
       const value = newsletterEmailInput.value;
       const xhr = new XMLHttpRequest();
       const url =
-        'https://khsw78qkle.execute-api.us-east-2.amazonaws.com/Stage/newsletter?token=db1f899025b5a59a76b6b34b2a013893';
+        'https://z93f42xq2l.execute-api.us-east-2.amazonaws.com/Stage/newsletter?token=db1f899025b5a59a76b6b34b2a013893';
       xhr.open('POST', url, true);
       xhr.setRequestHeader('Content-Type', 'application/json');
       xhr.onreadystatechange = function() {
