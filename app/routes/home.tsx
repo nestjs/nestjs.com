8e6d19085b655dcf95a89525db52615cbbb64486
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-11-25 11:27:26 +0100

    style: animate menu panel too

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -32,67 +32,69 @@ export default function Home() {
         <BokehHeader parentRef={headerRef} />
         <NoiseOverlay />
         <div className="container relative z-10">
-          <div
-            className={`${classes.navPanel} flex items-center p-5 bg-black/60 mt-8 rounded-[32px]`}
-          >
-            <div className="flex justify-start">
-              <a href="https://nestjs.com">
-                <img src="logo.svg" alt="NestJS Logo" className="h-10" />
-              </a>
-            </div>
-
-            <nav
-              className={`flex justify-center space-x-10 font-medium text-base flex-1`}
+          <BlurIn delay={0.1}>
+            <div
+              className={`${classes.navPanel} flex items-center p-5 bg-black/60 mt-8 rounded-[32px]`}
             >
-              <a href="https://docs.nestjs.com">Docs</a>
-              <a href="https://courses.nestjs.com">Official courses</a>
-              <a href="#">Tools</a>
-              <a href="https://enterprise.nestjs.com">Enterprise</a>
-              <a href="https://jobs.nestjs.com">Jobs</a>
-            </nav>
+              <div className="flex justify-start">
+                <a href="https://nestjs.com">
+                  <img src="logo.svg" alt="NestJS Logo" className="h-10" />
+                </a>
+              </div>
 
-            <div className="flex justify-end space-x-5">
-              <a
-                href="https://github.com/nestjs/nest"
-                target="_blank"
-                className="icon m-l-30"
+              <nav
+                className={`flex justify-center space-x-10 font-medium text-base flex-1`}
               >
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5 fill-current color-text"
+                <a href="https://docs.nestjs.com">Docs</a>
+                <a href="https://courses.nestjs.com">Official courses</a>
+                <a href="#">Tools</a>
+                <a href="https://enterprise.nestjs.com">Enterprise</a>
+                <a href="https://jobs.nestjs.com">Jobs</a>
+              </nav>
+
+              <div className="flex justify-end space-x-5">
+                <a
+                  href="https://github.com/nestjs/nest"
+                  target="_blank"
+                  className="icon m-l-30"
                 >
-                  <path d={siGithub.path} />
-                </svg>
-              </a>
-              <a
-                href="https://twitter.com/nestframework"
-                target="_blank"
-                className="icon"
-              >
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5 color-text fill-current"
+                  <svg
+                    role="img"
+                    viewBox="0 0 24 24"
+                    className="w-5 h-5 fill-current color-text"
+                  >
+                    <path d={siGithub.path} />
+                  </svg>
+                </a>
+                <a
+                  href="https://twitter.com/nestframework"
+                  target="_blank"
+                  className="icon"
                 >
-                  <path d={siX.path} />
-                </svg>
-              </a>
-              <a
-                href="https://linkedin.com/company/19078346"
-                target="_blank"
-                className="icon"
-              >
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5 fill-current color-text"
+                  <svg
+                    role="img"
+                    viewBox="0 0 24 24"
+                    className="w-5 h-5 color-text fill-current"
+                  >
+                    <path d={siX.path} />
+                  </svg>
+                </a>
+                <a
+                  href="https://linkedin.com/company/19078346"
+                  target="_blank"
+                  className="icon"
                 >
-                  <path d={siDiscord.path} />
-                </svg>
-              </a>
+                  <svg
+                    role="img"
+                    viewBox="0 0 24 24"
+                    className="w-5 h-5 fill-current color-text"
+                  >
+                    <path d={siDiscord.path} />
+                  </svg>
+                </a>
+              </div>
             </div>
-          </div>
+          </BlurIn>
           <div className="centered text-center pt-30 pb-40 flex flex-col items-center">
             <h1 className="text-8xl font-medium max-w-4xl self-center">
               <WordByWord>More than just a Node framework</WordByWord>
