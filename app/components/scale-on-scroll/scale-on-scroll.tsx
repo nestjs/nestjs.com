884eda60ae8184f652179b1d1a4c921b03aef00f
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-11-27 18:06:26 +0100

    chore: turn off markers

--- app/components/scale-on-scroll/scale-on-scroll.tsx
+++ app/components/scale-on-scroll/scale-on-scroll.tsx
@@ -22,7 +22,7 @@ export function ScaleOnScroll({ children }: { children: React.ReactNode }) {
       start: "top top",
       end: "bottom top",
       scrub: true,
-      markers: true,
+      // markers: true,
       onUpdate: () => {
         const containerRect = container.getBoundingClientRect();
 
